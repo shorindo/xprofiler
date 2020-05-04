421dc3deb132e16374e3e8f9429248603fb97403
Kazuyuki Matsuda <kazm@shorindo.com>
2020-05-04 15:19:16 +0900

    add some resources.

---
+++ src/test/resources/settings.js
@@ -0,0 +1,17 @@
+settings = {
+    includes : [
+        "com.shorindo.*"
+    ],
+    excludes : [
+        "com.shorindo.docs.action.ActionContext",
+        "com.shorindo.docs.action.ActionLogger",
+        "com.shorindo.docs.repository.DatabaseSchema$Column",
+        "com.shorindo.docs.repository.RepositoryStatement$ColumnMapper",
+        "com.shorindo.docs.repository.QueryStatement.*",
+        "com.shorindo.docs.BeanUtil",
+        "com.shorindo.docs.action.ActionMessages$Util",
+        "com.shorindo.docs.repository.QueryStatement",
+        "com.shorindo.docs.specout.SpecoutEntity*",
+        "com.shorindo.docs.document.DocumentEntity"
+    ]
+}
